19d126723a08b6347a6a7a9ce7c87807e3dda146
Josh Taylor <jostaylor@chapman.edu>
2019-11-21 02:22:04 +0000

    NO WAY ITS MY FIRST COMMITgit status1! I added our main.cpp and added some helpful instructional comments based on our brainstrorm sesh. Super s1ck!

--- Faculty.h
+++ Faculty.h
@@ -27,23 +27,3 @@ Faculty::Faculty(int student)
 {
   m_advisees.push(student);
 }
-
-
-
-
-
-Student Records
-Student records will be stored in a Student class. Student records contain a unique
-student ID (an integer), a String name field, a string level field (Freshman, Sophomore,
-etc), a String major field, a double GPA field, and an integer advisor field, which will
-contain the Faculty ID of their advisor. These are the only fields the class contains.
-The Student class must overload equality, less than, greater than operators, etc. so that we
-can compare them to one another.
-
-
-Faculty Records
-
-Faculty records are similar to student records, and will also require overloaded operators.
-Faculty records contain an integer Faculty ID, a String name, a String level (lecturer,
-assistant prof, associate prof, etc), a String department, and a list of integers
-corresponding to all of the faculty member’s advisees’ ids.
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+// Assign 5 todo list:
+/*
+1. 2 BSTs that act as our two tables for students and faculty respectively (masterFaculty and masterStudent)
+---MENU---
+2.  Print print print, order the stuff
+3. Add new student and add new advisor
+4. Delete student and delete advisor --> must adjust accordingly for advisee list and such
+5. Misc functons for menu
+6. Rollback --> stack
+---Structure of main---
+7. Check current directory for facultyTable and studentTable
+8. If neither of them exist, Initialize two new empty trees, otherwise, read file data into tree objects
+9. Run menu loop until user exits
+10. After exiting, write/serialize trees to facultyTable and studentTable files
+*/
+
+// Other shtuff:
+// Convert BST to GenBST that finishes the implementation and makes it generic
+// Extend person class
+
+
+int main(int argc, char** argv){
+  cout << "test main" << endl;
+
+  // do
+  // the
+  // things
+
+  return 0;
+}
